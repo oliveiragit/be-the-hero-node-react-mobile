ec148695d629abd84f88bf31513d79d62e590fc9
oliveiragit <deoliveirax@hotmail.com>
2020-10-24 14:16:39 -0300

    fix

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -4,7 +4,7 @@ import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export default function NewIncident() {
   const [title, setTitle] = useState('');
@@ -34,19 +34,17 @@ export default function NewIncident() {
 
   return (
     <Container>
-      <section>
-        <img src={logoImg} alt="Be the Hero logo" />
+      <Content>
+        <section>
+          <img src={logoImg} alt="Be the Hero logo" />
 
-        <h1>Cadastrar novo caso</h1>
-        <p>
-          Descreva o caso detalhadamente para encotrar um herói para resolvê-lo.
-        </p>
-        <Link to="/profile">
-          <FiArrowLeft size={16} color="#E02041" />
-          Voltar
-        </Link>
-      </section>
-      <section>
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para
+            resolvê-lo.
+          </p>
+        </section>
+
         <form onSubmit={handlerSubmit}>
           <input
             placeholder="Titulo"
@@ -65,7 +63,11 @@ export default function NewIncident() {
           />
           <button type="submit">Cadastrar</button>
         </form>
-      </section>
+      </Content>
+      <Link to="/profile">
+        <FiArrowLeft size={16} color="#E02041" />
+        Voltar
+      </Link>
     </Container>
   );
 }
